2d6ce680ba5d63f35640ea44ef188b081e7cae21
ThatLolaSnail <thatlolasnail@gmail.com>
2024-05-30 23:00:45 +0200

    First working template test

--- src/backend/src/frontend/frontend.controller.ts
+++ src/backend/src/frontend/frontend.controller.ts
@@ -1,15 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger } from '@nestjs/common';
 
 @Controller('frontend')
 export class FrontendController {
 
+
     @Get("/")
     getFrontend() {
+	let logger = new Logger("Frontend.Controller");
 	let ejs = require('ejs');
 	var read = require('fs').readFileSync;
+	
+	var options = {encoding: 'utf-8', flag: 'r'};
+	var filename = "test.html";
+	var data = read(filename, options);
+
 	let people = ['geddy', 'neil', 'alex'];
-	//let html = ejs.render(read("/home/lola/Documents/test.html"), {people: people});
-	let html = ejs.render('<%= people.join(", "); %>', {people: people});
+	let html = ejs.render(data, {people: people});
+	logger.log("html: " + html);
+	//let html = ejs.render('<%= people.join(", "); %>', {people: people});
 	//let html = ejs.compile("test.html", {people: people});
         return html; //read('test.html');//html;
     }
