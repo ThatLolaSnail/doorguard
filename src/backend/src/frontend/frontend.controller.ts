1c42fedf6f7f49bb61b2afa753fe0c7788fad053
ThatLolaSnail <thatlolasnail@gmail.com>
2024-06-06 12:00:55 +0200

    Add index.html

--- src/backend/src/frontend/frontend.controller.ts
+++ src/backend/src/frontend/frontend.controller.ts
@@ -6,19 +6,19 @@ export class FrontendController {
 
     @Get("/")
     getFrontend() {
+	let people = ['geddy', 'neil', 'alex'];
+        return this.render("html/index.html", {people: people});
+    }
+    render(filepath: string, data){
 	let logger = new Logger("Frontend.Controller");
 	let ejs = require('ejs');
 	var read = require('fs').readFileSync;
+	logger.log("Filename: "+filepath);
 	
 	var options = {encoding: 'utf-8', flag: 'r'};
-	var filename = "test.html";
-	var data = read(filename, options);
-
-	let people = ['geddy', 'neil', 'alex'];
-	let html = ejs.render(data, {people: people});
+	var template = read(filepath, options);
+	let html = ejs.render(template, data);
 	logger.log("html: " + html);
-	//let html = ejs.render('<%= people.join(", "); %>', {people: people});
-	//let html = ejs.compile("test.html", {people: people});
-        return html; //read('test.html');//html;
+	return html;
     }
 }
--- src/backend/src/frontend/frontend.service.ts
+++ src/backend/src/frontend/frontend.service.ts
@@ -2,14 +2,14 @@ import { Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
 export class FrontendService {
-    private readonly logger = new Logger("Frontend");
+    //private readonly logger = new Logger("Frontend");
 
     constructor(){
-	this.logger.log("Hello World");
-	let ejs = require('ejs');
-	let people = ['geddy','Fritz'];
-	let html = ejs.render('<% people.join(", "); %>', {people: people});
-	this.logger.log(html);
+	//this.logger.log("Hello World");
+	//let ejs = require('ejs');
+	//let people = ['geddy','Fritz'];
+	//let html = ejs.render('<% people.join(", "); %>', {people: people});
+	//this.logger.log(html);
 
     }
 }
