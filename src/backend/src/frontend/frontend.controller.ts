1c7985a4b89ab6bdc30e139c462b5133b94e66a7
Alexandyva <alexandrakohne11@gmail.com>
2024-06-06 12:11:45 +0200

    FrontEnd Controller fix

--- src/backend/src/frontend/frontend.controller.ts
+++ src/backend/src/frontend/frontend.controller.ts
@@ -1,24 +1,14 @@
 import { Controller, Get, Logger } from '@nestjs/common';
+import { FrontendService } from './frontend.service';
 
 @Controller('frontend')
 export class FrontendController {
 
+	constructor(private service: FrontendService){}
 
     @Get("/")
     getFrontend() {
-	let people = ['geddy', 'neil', 'alex'];
-        return this.render("html/index.html", {people: people});
+		return this.service.lolaFunction();
     }
-    render(filepath: string, data){
-	let logger = new Logger("Frontend.Controller");
-	let ejs = require('ejs');
-	var read = require('fs').readFileSync;
-	logger.log("Filename: "+filepath);
-	
-	var options = {encoding: 'utf-8', flag: 'r'};
-	var template = read(filepath, options);
-	let html = ejs.render(template, data);
-	logger.log("html: " + html);
-	return html;
-    }
+    
 }
--- src/backend/src/frontend/frontend.service.ts
+++ src/backend/src/frontend/frontend.service.ts
@@ -12,4 +12,22 @@ export class FrontendService {
 	//this.logger.log(html);
 
     }
+
+	lolaFunction() {
+		let people = ['geddy', 'neil', 'alex'];
+        return this.render("html/index.html", {people: people});
+	}
+
+	render(filepath: string, data){
+		let logger = new Logger("Frontend.Controller");
+		let ejs = require('ejs');
+		var read = require('fs').readFileSync;
+		logger.log("Filename: "+filepath);
+		
+		var options = {encoding: 'utf-8', flag: 'r'};
+		var template = read(filepath, options);
+		let html = ejs.render(template, data);
+		logger.log("html: " + html);
+		return html;
+	}
 }
